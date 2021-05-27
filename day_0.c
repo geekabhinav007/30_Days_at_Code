5cdbf7ef94bd754f5a2ad738d66b2f1d1edd7012
Kumar Abhinav <76863411+geekabhinav007@users.noreply.github.com>
2021-05-27 16:41:01 +0530

    added comment

--- day_0.c
+++ day_0.c
@@ -1,4 +1,4 @@
-
+// I am abhinav
 #include <stdio.h>
 int main()
 {
@@ -6,6 +6,9 @@ char str[20];
 scanf("%[^\n]%*c", str);
 
 /* Here, [] is the scanset character. ^\n tells to take input until newline doesn’t get encountered. Then, with this %*c, it reads newline character and here used * indicates that this newline character is discarded.*/
+
+
+
 
 printf("Hello, World.\n");
 printf("%s", str);
